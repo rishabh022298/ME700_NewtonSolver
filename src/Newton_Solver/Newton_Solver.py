e438f3901cec3a463f0fa9eb040743334b0bb152
rishabh022298 <rishy@bu.edu>
2025-02-09 16:51:01 -0500

    Update Newton_Solver.py

--- src/Newton_Solver/Newton_Solver.py
+++ src/Newton_Solver/Newton_Solver.py
@@ -4,6 +4,7 @@ This file contains a few functions required to implement the Newton's Method
 
 import numpy as np
 import matplotlib.pyplot as plt
+from pathlib import Path
 from typing import Callable
 
 '''
